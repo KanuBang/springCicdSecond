d01ca587e39b3a9777b4ab3502c93b9f01291407
gdrffg <gdrffg@gmail.com>
2025-03-07 15:49:54 +0900

    init

---
+++ src/main/java/org/example/springcicdsecond/SpringCicdSecondApplication.java
@@ -0,0 +1,13 @@
+package org.example.springcicdsecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCicdSecondApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCicdSecondApplication.class, args);
+    }
+
+}
